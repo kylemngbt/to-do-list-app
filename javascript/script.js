6f4c3f3b4347058568470f3e925a18535dee2c9b
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-02 15:53:13 +0800

    feat: display the saved tasks

--- javascript/script.js
+++ javascript/script.js
@@ -33,3 +33,8 @@ function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
