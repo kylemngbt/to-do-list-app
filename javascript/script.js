3ed8d9b19f30932ad9b03c5da9a415b798046c67
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-02 15:37:22 +0800

    feat: create the addTask function

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,19 @@
 const inputBox = document.querySelector("#input-box");
 const listContainer = document.querySelector("#list-container");
 const addBtn = document.querySelector(".row button");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li .appendChild(span);
+  }
+
+  inputBox.value = "";
+}
+
